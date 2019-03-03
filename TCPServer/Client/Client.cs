70eea9f1ce7f8ec240a72537c42f827099941dcb
AlinaMisaki <alina.iuliana95@yahoo.com>
2019-03-03 20:37:50 +0200

    Homework 1 - TCP Client Server

---
+++ TCPServer/Client/Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            int port = 6001;
+            string serverAddress = "127.0.0.1";
+
+            TcpClient client = new TcpClient(serverAddress, port);
+
+            Console.WriteLine("Client is connected to address {0}", serverAddress);
+
+            string message = string.Empty;
+
+            while(message!=null && !message.StartsWith("EXIT"))
+            {
+                Console.WriteLine("Enter the ecuation in the form /'operand operator operand/'");
+                message = Console.ReadLine();
+
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(Encoding.ASCII.GetBytes(message), 0, message.Length);
+                Console.WriteLine("Ecuation {0} was sent to the server for processing.", message);
+
+                var receivedData = new byte[1024];
+
+                var response = stream.Read(receivedData, 0, receivedData.Length);
+
+                var receivedMessage = Encoding.ASCII.GetString(receivedData, 0, response);
+
+                Console.WriteLine("Answer = {0}", receivedMessage);
+
+            }
+
+            Console.WriteLine("Client connection is closed!");
+            client.GetStream().Dispose();
+            client.Dispose();
+        }
+    }
+}
---
+++ TCPServer/Server/Server.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+
+            try
+            {
+                int port = 6001;
+                IPAddress localhost = IPAddress.Parse("127.0.0.1");
+
+                server = new TcpListener(localhost, port);
+                server.Start();
+
+                Byte[] readingBytes = new Byte[256];
+                String receivedEcuation = null;
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a client connection ...");
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Client connected!");
+
+                    receivedEcuation = null;
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    while((i = stream.Read(readingBytes, 0, readingBytes.Length)) != 0)
+                    {
+                        receivedEcuation = System.Text.Encoding.ASCII.GetString(readingBytes, 0, i);
+
+                        receivedEcuation = receivedEcuation.ToUpper();
+
+                        if (receivedEcuation != "EXIT")
+                        {
+                            Console.WriteLine("Ecuation Received: {0}", receivedEcuation);
+
+                            int result = 0;
+                            byte[] msg;
+                            string[] ecuationElements = receivedEcuation.Split(' ');
+
+                            if (ecuationElements.Length == 3) {
+                                int operand1 = int.Parse(ecuationElements[0]);
+                                string operation = ecuationElements[1];
+                                int operand2 = int.Parse(ecuationElements[2]);
+
+                                switch (operation)
+                                {
+                                    case "+":
+                                        result = operand1 + operand2;
+                                        break;
+                                    case "-":
+                                        result = operand1 - operand2;
+                                        break;
+                                    case "*":
+                                        result = operand1 * operand2;
+                                        break;
+                                    case "/":
+                                        if (operand2 != 0)
+                                        {
+                                            result = operand1 / operand2;
+                                        }
+                                        else
+                                        {
+                                            result = 0;
+                                        }
+                                        break;
+                                }
+
+                                msg = System.Text.Encoding.ASCII.GetBytes(result.ToString());
+                                Console.WriteLine("Sending the result: {0}", result.ToString());
+                            }
+                            else
+                            {
+                                msg = System.Text.Encoding.ASCII.GetBytes("Invalid Input! Try again.");
+                                Console.WriteLine("Sending the result: Invalid Input! Try again.");
+                            }
+          
+                            stream.Write(msg, 0, msg.Length);
+                        }
+                        else
+                        {
+                            client.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception Occured: {0}", e.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+    }
+}
